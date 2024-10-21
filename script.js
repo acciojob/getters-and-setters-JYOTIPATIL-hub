c9ed892e7b71b5f619abe71dca99ef61056dc018
JYOTIPATIL-hub <142783664+JYOTIPATIL-hub@users.noreply.github.com>
2024-10-21 19:13:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,6 @@
-//complete this code
-
-// Do not change the code below this line
-class Person {
-    constructor(name, age) {
-        this._name = name;
-        this._age = age;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    set age(age) {
-        if (age >= 0) {
-            this._age = age;
-        } else {
-            console.error("Age cannot be negative.");
-        }
-    }
-}
-
-class Student extends Person {
-    study() {
-        console.log(`${this._name} is studying.`);
-    }
-}
-
-class Teacher extends Person {
-    teach() {
-        console.log(`${this._name} is teaching.`);
-    }
-}
-
-// Example usage:
 const person = new Person("John", 25);
 console.log(person.name); // Output: John
+
 person.age = 30;
 console.log(person.age); // Output: 30
 
